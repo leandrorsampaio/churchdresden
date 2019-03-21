afaeb7dd7472e558ef6c3081f6591ef43d09b7ca
Leandro Rossi Sampaio <leandro.sampaio@deptagency.com>
2019-03-21 18:04:40 +0100

    Some changes

---
+++ wp-dietrich-bonhoeffer/footer.php
@@ -0,0 +1,61 @@
+<footer class="footer">
+  <div class="footer-wraper default-width-wrapper">
+
+
+
+
+
+    <div class="footerlogos">
+      <div class="footerlogos-item logo-gge">
+        <img src="images/logo-gge.jpg" alt="GGE logo German"/>
+      </div>
+      <div class="footerlogos-item logo-ksp">
+        <img src="images/logo-ksp.png" alt="KSP logo German"/>
+      </div>
+    </div>
+
+
+
+
+
+
+
+    <span class="translated-content lang-de">
+      <p>
+        2019 - Alle Rechte vorbehalten
+      </p>
+    </span>
+    <span class="translated-content lang-en">
+      <p>
+        2019 - All rights reserved
+      </p>
+    </span>
+    <span class="translated-content lang-es">
+      <p>
+        2019 - Todos los derechos reservados
+      </p>
+    </span>
+
+
+
+
+
+
+
+
+
+  </div>
+</footer>
+
+
+
+
+
+</div><!-- class="wrapper" -->
+
+
+<?php wp_head(); ?> 
+
+</body>
+
+</html>
---
+++ wp-dietrich-bonhoeffer/header.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <?php wp_head(); ?> 
+	<meta charset="UTF-8">
+	<title>International Church Dresden</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+
+	<meta name="Description" content="International Protestant Church in Dresden Neustadt. Services in German, English and Spanish. ">
+	<meta name="Keywords" content="church, dresden, neustadt, christian, comunity, jesus God, small group, chill out, friendship, lutheran, leben, geminde, pastor, english, spanish, german, welcome, international, cill out, ">
+
+
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
+	<script src="js/app.js"></script>
+
+	<link href="https://fonts.googleapis.com/css?family=Archivo:400,400i,700" rel="stylesheet">
+	<link rel="stylesheet" href=style/font-awesome.min.css />
+
+
+
+	<script src="js/sss.js"></script>
+
+
+	<link rel="stylesheet/less" type="text/css" href="style/style.less" />
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/less.js/3.0.0/less.min.js" type="text/javascript"></script>
+
+
+
+
+	<script src="sss/sss.min.js"></script>
+	<script>
+	jQuery(function($) {
+		$('.slider').sss();
+	});
+	jQuery(function($) {
+		$('.slider2').sss();
+	});
+	</script>
+
+
+</head>
+
+<body>
+	<div class="wrapper">
