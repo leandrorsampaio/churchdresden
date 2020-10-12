5d8bcef259910a7d4d80bda39c5d93aff90550ef
Leandro Rossi Sampaio <leandro.sampaio@deptagency.com>
2020-10-12 23:51:40 +0200

    New font

--- wp-dietrich-bonhoeffer/header.php
+++ wp-dietrich-bonhoeffer/header.php
@@ -25,9 +25,9 @@
   <link rel="stylesheet" type="text/css" href="<?php bloginfo('template_url'); ?>/style.css" />
 
 	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
-		<script src="<?php bloginfo('template_url'); ?>/js/app.js"></script>
+	<script src="<?php bloginfo('template_url'); ?>/js/app.js"></script>
 
-	<link href="https://fonts.googleapis.com/css?family=Archivo:400,400i,700" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,700;0,800;1,400&display=swap" rel="stylesheet">
   
 
 
